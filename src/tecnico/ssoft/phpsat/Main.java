e1e7dcff7c6a4ced788e146973a87114096e9bd5
Helena Cruz <helcruz8@gmail.com>
2016-10-12 17:56:47 +0100

    main is printing all vulnerabilities

--- src/tecnico/ssoft/phpsat/Main.java
+++ src/tecnico/ssoft/phpsat/Main.java
@@ -1,9 +1,23 @@
 package tecnico.ssoft.phpsat;
 
+import tecnico.ssoft.phpsat.core.Vulnerability;
+import tecnico.ssoft.phpsat.parser.Parser;
+import tecnico.ssoft.phpsat.parser.VulnerabilitiesParser;
+
+import java.util.List;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("eeeeeeee");
+        Parser parser = new VulnerabilitiesParser();
+        parser.parse();
+
+        List<Vulnerability> result = parser.result();
+
+        for (Vulnerability v : result) {
+            System.out.println(v.toString());
+            System.out.println(" ");
+        }
     }
 }

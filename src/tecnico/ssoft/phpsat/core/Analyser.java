a068c8822b13ecb679f29c0e853b2aa3a6023e0b
Helena Cruz <helcruz8@gmail.com>
2016-11-14 23:42:07 +0000

    changed the way it is analysing the code. now it's better and simpler. there's still a bug in the sensitive sinks though

--- src/tecnico/ssoft/phpsat/core/Analyser.java
+++ src/tecnico/ssoft/phpsat/core/Analyser.java
@@ -4,202 +4,234 @@ import tecnico.ssoft.phpsat.parser.CodeParser;
 import tecnico.ssoft.phpsat.parser.VulnerabilitiesParser;
 import tecnico.ssoft.phpsat.parser.ast.*;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Analyser
 {
-    private boolean _vulnerable;
+    private boolean vulnerable;
 
-    private String _result;
+    private String result;
 
-    private List<Vulnerability> _vulnerabilities;
-    private List<String> _sanitizationFunctions;
-    private List<String> _sinks;
+    private List<Vulnerability> vulnerabilities;
+    private List<String> sanitizationFunctions;
+    private List<String> sinks;
 
-    private List<Node> _code;
-
-    private List<CodeVulnerability> _codeVulnerabilities;
+    private List<Node> code;
 
     public Analyser(String file)
     {
-        _vulnerable = false;
-        _codeVulnerabilities = new ArrayList<>();
-        _result = "";
+        vulnerable = false;
+        result = "";
 
         VulnerabilitiesParser vulnerabilitiesParser = new VulnerabilitiesParser();
         vulnerabilitiesParser.parse();
-        _vulnerabilities = vulnerabilitiesParser.result();
-        _sanitizationFunctions = vulnerabilitiesParser.getSanitizationFunctions();
-        _sinks = vulnerabilitiesParser.getSinks();
+        vulnerabilities = vulnerabilitiesParser.result();
+        sanitizationFunctions = vulnerabilitiesParser.getSanitizationFunctions();
+        sinks = vulnerabilitiesParser.getSinks();
 
         CodeParser codeParser = new CodeParser(file);
         codeParser.parse();
-        _code = codeParser.result();
-    }
-
-    public List<CodeVulnerability> getCodeVulnerabilities()
-    {
-        return _codeVulnerabilities;
+        code = codeParser.result();
     }
 
     public void analyse()
     {
-        analyseCode();
+        // analyseCode();
         analyseVulnerabilities();
-    }
-
-    public void analyseCode()
-    {
-        for (Node node : _code) {
-            if (node instanceof Assignment) {
-                Assignment assignment = (Assignment) node;
-                analyseAssignment(assignment);
-            }
-            else if (node instanceof Function) {
-                Function function = (Function) node;
-                analyseFunction(function);
-            }
-            else {
-                System.out.println("Should not happen... what am i?");
-                System.out.println(node.toString());
-            }
-        }
-    }
-
-    public void analyseAssignment(Assignment assignment)
-    {
-        /*
-         * let's start by the right value
-         * - variable: if it's tainted, then left value is tainted too
-         * - variable: if it comes from an entry point, tainted
-         * - function: analyse function
-         * - value: if it contains tainted variables, tainted
-         */
-        if (assignment.getRight() instanceof Variable) {
-            Variable variable = (Variable) assignment.getRight();
-            if (variable.isGlobal()) {
-                assignment.getLeft().taint();
-                addCodeVulnerability(assignment.getLeft(), variable);
-            }
-            if (!variable.isTainted()) {
-                assignment.getLeft().untaint();
-            }
-        }
-        else if (assignment.getRight() instanceof Value) {
-            Value value = (Value) assignment.getRight();
-            for (Variable variable : value.getVariables()) {
-                assignment.getLeft().untaint(); // if one of the variables are tainted then it will be tainted in the next step
-                if (variable.isTainted()) {
-                    addCodeVulnerability(assignment.getLeft(), variable);
-                    assignment.getLeft().taint();
-                }
-            }
-        }
-        else if (assignment.getRight() instanceof Function) {
-            Function function = (Function) assignment.getRight();
-            analyseFunction(function);
-        }
-    }
-
-    public void addCodeVulnerability(Variable variable1, Variable variable2)
-    {
-        CodeVulnerability codeVulnerability = new CodeVulnerability(variable1);
-        codeVulnerability.addEntryPoint(variable2.getGlobalName());
-        variable1.setGlobal(true);
-        variable1.setGlobalName(variable2.getGlobalName());
-        _codeVulnerabilities.add(codeVulnerability);
-    }
-
-    public void addCodeVulnerability(Function function, Variable variable, boolean isSink)
-    {
-        CodeVulnerability codeVulnerability = new CodeVulnerability(variable);
-        if (isSink) {
-            codeVulnerability.addSink(function.getName());
-        }
-        else {
-            codeVulnerability.addSanitizationFunction(function.getName());
-        }
-        if (variable.isGlobal()) {
-            codeVulnerability.addEntryPoint(variable.getGlobalName());
-        }
-        _codeVulnerabilities.add(codeVulnerability);
-    }
-
-    public void analyseFunction(Function function)
-    {
-        if (function.isThisFunction(_sinks)) {
-            for (RightValue rv : function.getArgs()) {
-                if (rv instanceof Variable) {
-                    Variable variable = (Variable) rv;
-                    addCodeVulnerability(function, variable, true);
-                }
-            }
-        }
-        else if (function.isThisFunction(_sanitizationFunctions)) {
-            for (RightValue rv : function.getArgs()) {
-                if (rv instanceof Variable) {
-                    Variable variable = (Variable) rv;
-                    addCodeVulnerability(function, variable, false);
-                }
-            }
-        }
-        // else do nothing
-    }
-
-    public void analyseVulnerabilities()
-    {
-        for (Vulnerability vulnerability : _vulnerabilities) {
-            for (CodeVulnerability codeVulnerability : _codeVulnerabilities) {
-                // they have an entry point in common
-                if (!Collections.disjoint(vulnerability.getEntryPoints(), codeVulnerability.getEntryPoints())) {
-                    // they have a sink in common
-                    if (!Collections.disjoint(vulnerability.getSinks(), codeVulnerability.getSinks())) {
-                        // if they don't have a sanitization function in common, oopsy, its vulnerable
-                        if (Collections.disjoint(vulnerability.getSanitizationFunctions(),
-                                codeVulnerability.getSanitizationFunctions())) {
-                            if (_vulnerable) {
-                                _result += "- " + vulnerability.type() + "\n";
-                            }
-                            else {
-                                _vulnerable = true;
-                                _result = vulnerability.type() + "\n";
-                            }
-                        }
-                        else {
-                            if (!_vulnerable) {
-                                _result += inCommon(vulnerability.getSanitizationFunctions(),
-                                        codeVulnerability.getSanitizationFunctions()) + "\n";
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    public String inCommon(List<String> list1, List<String> list2)
-    {
-        for (String string1 : list1) {
-            for (String string2 : list2) {
-                if (string1.equals(string2)) {
-                    return string1;
-                }
-            }
-        }
-
-        return null;
     }
 
     public String result()
     {
-        if (_vulnerable) {
-            return "The program is vulnerable to:\n" + _result;
+        if (result.isEmpty()) {
+            return "This program is safe.";
         }
-        else {
-            return "The program is not vulnerable because of the following sanitization functions:\n" + _result;
+        return result;
+    }
+
+    private void analyseVulnerabilities()
+    {
+        // printCode();
+
+        for (Vulnerability vulnerability : vulnerabilities) {
+            taintAllVariables(code); // start with everything tainted
+
+            for (Node node : code) {
+                if (node instanceof Assignment) {
+                    Assignment assignment = (Assignment) node;
+                    RightValue rightValue = assignment.getRight();
+                    if (rightValue instanceof Variable) {
+                        Variable variable = (Variable) rightValue;
+                        if (variable.isTainted()) {
+                            assignment.getLeft().taint();
+                        }
+                        else {
+                            assignment.getLeft().untaint();
+                        }
+                    }
+                    else if (rightValue instanceof Function) {
+                        Function function = (Function) rightValue;
+                        analyseFunction(function, vulnerability);
+                    }
+                    else if (rightValue instanceof Value) {
+                        Value value = (Value) rightValue;
+                        value.untaint();
+                        for (Variable variable : value.getVariables()) {
+                            if (variable.isTainted() ) {
+                                value.taint();
+                                assignment.getLeft().taint();
+                                break;
+                            }
+                        }
+                        if (value.isTainted()) {
+                            assignment.getLeft().taint();
+                        }
+                        else {
+                            assignment.getLeft().untaint();
+                        }
+                    }
+                }
+                else if (node instanceof Function) {
+                    Function function = (Function) node;
+                    analyseFunction(function, vulnerability);
+                }
+            }
         }
+    }
+
+    private void taintAllVariables(List<Node> code)
+    {
+        for (Node node : code) {
+            if (node instanceof Assignment) {
+                Assignment assignment = (Assignment) node;
+                assignment.getLeft().taint();
+                if (assignment.getRight() instanceof Variable) {
+                    Variable variable = (Variable) assignment.getRight();
+                    variable.taint();
+                }
+                else if (assignment.getRight() instanceof Value) {
+                    Value value = (Value) assignment.getRight();
+                    for (Variable variable : value.getVariables()) {
+                        variable.taint();
+                    }
+                }
+            }
+            else if (node instanceof Variable) {
+                Variable variable = (Variable) node;
+                variable.taint();
+            }
+            else if (node instanceof Value) {
+                Value value = (Value) node;
+                for (Variable variable : value.getVariables()) {
+                    variable.taint();
+                }
+            }
+        }
+    }
+
+    private void analyseFunction(Function function, Vulnerability vulnerability)
+    {
+        boolean vulnerable = false;
+
+        if (function.isThisFunction(vulnerability.getSanitizationFunctions())) {
+            for (RightValue rv : function.getArgs()) {
+                rv.untaint();
+            }
+            function.untaint();
+        }
+        else if (function.isThisFunction(vulnerability.getSinks())) {
+            function.untaint();
+            for (RightValue rv : function.getArgs()) {
+                if (rv.isTainted()) {
+                    function.taint();
+                    vulnerable = true;
+                }
+            }
+        }
+
+        if (vulnerable) {
+            addVulnerability(function, vulnerability);
+        }
+    }
+
+    private void addVulnerability(Function function, Vulnerability vulnerability)
+    {
+        result += "The code is vulnerable to " + vulnerability.type() + " on function " +
+                function.getName() + " because of the args: ";
+
+        for (RightValue rightValue : function.getArgs()) {
+            if (rightValue instanceof Variable) {
+                Variable variable = (Variable) rightValue;
+                if (variable.isTainted()) {
+                    result += variable.getName() + ", ";
+                }
+            }
+            else if (rightValue instanceof Function) {
+                Function functionArg = (Function) rightValue;
+                if (functionArg.isTainted()) {
+                    result += functionArg.getName() + ", ";
+                }
+            }
+            else if (rightValue instanceof Value) {
+                Value value = (Value) rightValue;
+                for (Variable variable : value.getVariables()) {
+                    if (variable.isTainted()) {
+                        result += variable.getName() + ", ";
+                    }
+                }
+            }
+        }
+
+        result = result.substring(0, result.length() - 2);
+
+        result += ".\n";
+    }
+
+    private void printCode()
+    {
+        System.out.println("START\n");
+        for (Node node : code) {
+            if (node instanceof Assignment) {
+                System.out.println("START ASSIGNMENT");
+                Assignment assignment = (Assignment) node;
+                System.out.println(assignment.getLeft().toString());
+                if (assignment.getRight() instanceof Variable) {
+                    Variable variable = (Variable) assignment.getRight();
+                    System.out.println(variable);
+                }
+                else if (assignment.getRight() instanceof Value) {
+                    Value value = (Value) assignment.getRight();
+                    for (Variable variable : value.getVariables()) {
+                        System.out.println(value.toString());
+                    }
+                }
+                else if (assignment.getRight() instanceof Function) {
+                    Function function = (Function) assignment.getRight();
+                    System.out.println(function);
+                }
+                System.out.println("END ASSIGNMENT\n");
+            }
+            else if (node instanceof Variable) {
+                System.out.println("START VARIABLE");
+                Variable variable = (Variable) node;
+                System.out.println(variable);
+                System.out.println("END VARIABLE\n");
+            }
+            else if (node instanceof Value) {
+                System.out.println("START VALUE");
+                Value value = (Value) node;
+                for (Variable variable : value.getVariables()) {
+                    System.out.println(variable);
+                }
+                System.out.println("END VALUE\n");
+            }
+            else if (node instanceof Function) {
+                System.out.println("START FUNCTION");
+                Function function = (Function) node;
+                System.out.println(function);
+                System.out.println("END FUNCTION\n");
+            }
+        }
+        System.out.println("\nEND");
     }
 
 }

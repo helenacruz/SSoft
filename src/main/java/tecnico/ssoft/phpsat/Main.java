ae3e7ef2dfcf9b5c16105be87bdcaecea0098793
Helena Cruz <helcruz8@gmail.com>
2016-11-18 22:33:24 +0000

    made it more visible when running maven

--- src/main/java/tecnico/ssoft/phpsat/Main.java
+++ src/main/java/tecnico/ssoft/phpsat/Main.java
@@ -11,18 +11,26 @@ public class Main
         try {
             Analyser analyser = new Analyser(args[0]);
             analyser.analyse();
-            System.out.println(analyser.result());
+            System.out.println();
+            System.out.println("-> " + analyser.result());
+            System.out.println();
         }
         catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Missing an argument - please input the name of a file.");
+            System.out.println();
+            System.out.println("-> Missing an argument - please input the name of a file.");
+            System.out.println();
             System.exit(1);
         }
         catch (IOException e) {
-            System.out.println("File not found: " + args[0]);
+            System.out.println();
+            System.out.println("-> File not found: " + args[0]);
+            System.out.println();
             System.exit(1);
         }
         catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.out.println();
+            System.out.println("-> " + e.getMessage());
+            System.out.println();
             System.exit(1);
         }
     }
